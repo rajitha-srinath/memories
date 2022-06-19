45dbe9e3c94aaf0b1f0028179e147c6a63f44c76
rajitha-srinath <s.rajitha@directfn.com>
2022-06-19 16:56:51 +0530

    initialize server and client

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
+
+// App
+const app = express();
+
+// DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('DB connected'));
+
+// Middleware
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
