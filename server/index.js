09fddf3f4d96e76b66709f6d1fcd8da92bd3fe24
rajitha-srinath <s.rajitha@directfn.com>
2022-06-20 09:16:37 +0530

    Initialize posts routes and controllers

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 dotenv.config();
 
+import postRoutes from './routes/posts.js';
+
 // App
 const app = express();
 
@@ -18,6 +20,8 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 8000;
 
